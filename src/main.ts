44def14f15de167e64799b47d0ecae13035949f7
Deyvid0104 <driotol124@g.educaand.es>
2025-05-14 14:00:36 +0200

    main

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors(); // Habilita CORS para permitir solicitudes de diferentes dominios
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  }); // Habilita CORS para permitir solicitudes de diferentes dominios
   await app.listen(process.env.PUERTO ?? 4000);
 }
 bootstrap();
