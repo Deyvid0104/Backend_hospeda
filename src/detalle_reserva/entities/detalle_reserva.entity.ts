5bedd3f8b477986fc3ce5d3489858289868e7a9e
Deyvid0104 <driotol124@g.educaand.es>
2025-06-02 13:39:15 +0200

    Modificacion entity detalle_reserva

--- src/detalle_reserva/entities/detalle_reserva.entity.ts
+++ src/detalle_reserva/entities/detalle_reserva.entity.ts
@@ -1,34 +1,28 @@
-import { Entity, Column, PrimaryGeneratedColumn, ManyToOne } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne, JoinColumn } from 'typeorm';
 import { Reserva } from 'src/reserva/entities/reserva.entity';
 import { Habitacion } from 'src/habitacion/entities/habitacion.entity';
 
 @Entity()
 export class DetalleReserva {
- // Identificador único del detalle de reserva
- @PrimaryGeneratedColumn()
- id_detalle: number;
+  // Identificador único del detalle de reserva
+  @PrimaryGeneratedColumn()
+  id_detalle: number;
 
- // Identificador de la reserva asociada
- @Column()
- id_reserva: number;
+  // Relación ManyToOne con la entidad Reserva
+  @ManyToOne(() => Reserva, reserva => reserva.detalles_reserva)
+  @JoinColumn({ name: 'id_reserva' })
+  reserva: Reserva;
 
- // Identificador de la habitación asociada
- @Column()
- id_habitacion: number;
+  // Relación ManyToOne con la entidad Habitacion
+  @ManyToOne(() => Habitacion, habitacion => habitacion.detalles_reserva)
+  @JoinColumn({ name: 'id_habitacion' })
+  habitacion: Habitacion;
 
- // Número de noches reservadas
- @Column()
- noches: number;
+  // Número de noches reservadas
+  @Column()
+  noches: number;
 
- // Precio aplicado para la reserva
- @Column({ type: 'decimal', precision: 10, scale: 2 })
- precio_aplicado: number;
-
- // Relación ManyToOne con la entidad Reserva
- @ManyToOne(() => Reserva, reserva => reserva.detalles_reserva)
- reserva: Reserva;
-
- // Relación ManyToOne con la entidad Habitacion
- @ManyToOne(() => Habitacion, habitacion => habitacion.detalles_reserva)
- habitacion: Habitacion;
+  // Precio aplicado para la reserva
+  @Column({ type: 'decimal', precision: 10, scale: 2 })
+  precio_aplicado: number;
 }
